1c82b8dbc210052f1f7d4111f38bb7bebee4f794
Troy W <pyrodie18@gmail.com>
2019-12-16 20:32:48 -0500

    Create PartA.py

---
+++ Day4/PartA.py
@@ -0,0 +1,23 @@
+total = 0
+for n in range (124075, 580769):
+	num = [int(i) for i in str(n)]
+	adjacent = 0
+	decrease = 1
+	
+	#Check for adjacent values
+	for i in range (0, 5):
+		if num[i] == num[i+1]:
+			adjacent = 1
+			break
+	
+	#Check for decrease
+	for i in range (0, 5):
+		if num[i+1] < num[i]:
+			decrease = 0
+			break
+	
+	if adjacent and decrease:
+		total += 1
+		print(n)
+		
+print(total)
